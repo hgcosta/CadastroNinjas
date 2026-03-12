9b92892ac39a689729eb51e4c382c9163adbfedd
Hugo Costa <hugocosta.devs@gmail.com>
2026-03-12 08:56:44 -0300

    criacao do controller teste

---
+++ src/main/java/dev/hugocosta/CadastroNinjas/Controller.java
@@ -0,0 +1,17 @@
+package dev.hugocosta.CadastroNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Anotação para informar que é um controller
+@RequestMapping // Anotação para rotas
+public class Controller {
+
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é a minha primeira mensagem nessa rota";
+    }
+}
