297d5d09e43494b02f6be8e40ce809a237adb914
Hugo Costa <hugocosta.devs@gmail.com>
2026-04-23 20:45:00 -0300

    feat: atualiza o controller com mock dos endpoints principais dos ninjas

--- src/main/java/dev/hugocosta/CadastroNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/hugocosta/CadastroNinjas/Ninjas/NinjaController.java
@@ -1,16 +1,39 @@
 package dev.hugocosta.CadastroNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // Anotação para informar que é um controller
 @RequestMapping // Anotação para rotas
 public class NinjaController {
 
-
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é a minha primeira mensagem nessa rota";
+    }
+
+    // Adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja Criado";
+    }
+    // Listar os ninjas (READ)
+    @GetMapping("/todos")
+    public String monstrarTodosOsNinjas(){
+        return "Mostrar todos os ninjas";
+    }
+    // Procurar Ninja (READ)
+    @GetMapping("/todosId")
+    public String mostrarTodosNinjaPorId(){
+        return "Mostrar todos os ninjas por id";
+    }
+    // Alterar ninja (UPDATE)
+    @PutMapping("/alterarId")
+    public String alterarNinjaPorId(){
+        return "Alterar ninja por Id";
+    }
+    // Deletar ninja (DELETE)
+    @DeleteMapping("/deletarId")
+    public String deletarNinjaPorId(){
+        return "Deletar ninja por id";
     }
 }
